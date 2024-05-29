ba1b356b4abc780caaa0e00c90ce4d157b068265
BenW101 <n11960329@qut.edu.au>
2024-05-29 12:15:24 +1000

    Added Shield Wizard w/o particles
    Also changed Demon dude

---
+++ Assets/Abe Wizard/Wizard Attack Script.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WizardAttackScript : MonoBehaviour
+{
+    public Animator anim;
+    public ParticleSystem particles;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Attack1());
+    }
+
+
+    IEnumerator Attack1()
+    {
+        anim.SetBool("Attack1", true);
+        yield return new WaitForSeconds(1);
+        particles.Play();
+        yield return new WaitForSeconds(1);
+        anim.SetBool("Attack1", false);
+        yield return new WaitForSeconds(5);
+        StartCoroutine(Attack1());
+    }
+}
